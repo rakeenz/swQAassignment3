d6c247ee2d0212072e12a7b20b2a0b2051fc2c04
rakeenz <rsz12@msstate.edu>
2022-04-12 18:24:45 -0500

    test commit for CI

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def calc_category(bmi):
 
 
 
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
@@ -50,41 +51,3 @@ if __name__ == "__main__":
 
 
 
-
-# old code from assignment 2
-
-
-
-#def bmicalc():
-#    height = float(input("input height in inches: "))
-
- #   weight = float(input("input weight in pounds: "))
-
-#    weight = weight * 0.45
-
- #   height = height * 0.025
-
- #   height = height * height
-
- #   bmi = round((weight/height), 1)
-
-#   print("your BMI is: ")
- #   print(bmi)
-  #  if bmi < 18.5:
- #       print("Category: Underweight")   
- #   elif bmi >= 18.5 and bmi < 25: 
- #       print("Category: Normal Weight")
-  #  elif bmi >= 25 and bmi < 30: 
-  #      print("Category: Overweight")
-  #  elif bmi >= 30: 
-  #      print("Category: Obese")
-
- #   rs = input("Would you like to restart? (type y or n) ")
-  #  if rs == 'y':
-   #     bmicalc()
- #   else:
- #       exit()
-
-
-
-#bmicalc()
